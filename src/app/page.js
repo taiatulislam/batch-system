e59ccc40c0ea9bdcded693d664937cdedfe56b11
taiatulislam <taiatulislamapon@gmail.com>
2024-06-13 13:06:46 +0600

    features section complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Banner from "@/components/Banner/Banner";
+import Features from "@/components/Features/Features";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="max-w-7xl mx-auto">
       <Navbar></Navbar>
       <Banner></Banner>
+      <Features></Features>
       <Footer></Footer>
     </div>
   );
